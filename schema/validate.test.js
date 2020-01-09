7964054502bbe9bd53385edce60a1cd39c79d050
Shedrach Okonofua <shedrachokonofua@gmail.com>
2020-01-08 19:23:20 -0500

    Update schema

--- schema/validate.test.js
+++ schema/validate.test.js
@@ -1,38 +1,9 @@
 const test = require('ava');
 const validate = require('./validate');
+const fixture = require('./sample.json');
 
 test('schema - validate - should pass valid data', t => {
-	const testData = {
-		contact: {
-			name: 'Test Subject',
-			role: 'Test Role',
-			email: 'test@test.com',
-			phone: '2898009000',
-			website: 'https://test.com',
-			github: 'test',
-		},
-		skills: {
-			'programming-languages': ['Javascript'],
-			'libraries-framworks': ['React'],
-			'tools-platforms': ['Docker'],
-			methodologies: ['TDD'],
-		},
-		expirience: [
-			{
-				company: 'Test Company',
-				start: '2019-01-01',
-				end: '2019-02-01',
-				role: 'Test Role',
-				accomplishments: ['Tested things'],
-			},
-		],
-		education: {
-			degree: 'B.Eng Testing',
-			school: 'Test College',
-		},
-	};
-
-	const valid = validate(testData);
+	const valid = validate(fixture);
 	if (!valid) console.log(validate.errors);
 	t.true(valid);
 });
